22721666fd794730f01cb77607e8e40f3c6f26d3
David Schousen <dschousen@comcast.net>
2019-07-25 14:07:35 -0500

    Serialize the session data

--- app.js
+++ app.js
@@ -3,50 +3,26 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var apiUsersRouter = require('./routes/api/users');
-var config = require('./config.dev');
-
-var app = express();
 var mongoose = require('mongoose');
-//~line 7 after mongoose
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
 var passport = require('passport');
-
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var apiUsersRouter = require('./routes/api/users');
+var app = express();
+//Call the config file
+var config = require('./config.dev');
+//Connect to MongoDB
+mongoose.connect(config.mongodb, { useNewUrlParser: true });
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/api/users', apiUsersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
 app.use(require('express-session')({
   //Define the session store
   store: new MongoStore({
@@ -67,8 +43,33 @@ app.use(require('express-session')({
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.serializeUser(function(user, done){
+  done(null,{
+    id: user._id,
+    username: user.username,
+    email: user.email,
+    first_name: user.first_name,
+    last_name: user.last_name
+  });
+});
 
-module.exports = app;
+passport.deserializeUser(function(user, done){
+  done(null, user);
+});
 
-//Connect to MongoDB
-mongoose.connect(config.mongodb, { useNewUrlParser: true });
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/api/users', apiUsersRouter);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
