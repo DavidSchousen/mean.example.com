c5179ea0c1c58b203efb336dbb11c451fe735967
David Schousen <dschousen@comcast.net>
2019-07-29 16:40:28 -0500

    Last working version lol.

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 var Users = require('../../models/users');
 
-router.get('/:userId', function(req,res){
+router.get('/', function(req,res,next){
+  Users.findOne({}, function(err, users){
+     if(err){
+      return res.json({'success':false, 'error': err});
+    }
+     return res.json({'success':true, 'users': users});
+   });
+ });
 
+router.get('/:userId', function(req,res){
 var userId = req.params.userId;
     Users.findOne({'_id':userId}, function(err, user){
     if(err){
